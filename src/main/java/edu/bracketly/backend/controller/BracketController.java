3284c4dfd43e25e42b58834750fb2344f77bf4a1
Piotr Borczyk <howorang@gmail.com>
2017-12-14 23:44:19 +0100

    Added path to Bracket controller

--- src/main/java/edu/bracketly/backend/controller/BracketController.java
+++ src/main/java/edu/bracketly/backend/controller/BracketController.java
@@ -6,9 +6,11 @@ import edu.bracketly.backend.repository.BracketRepository;
 import edu.bracketly.backend.service.BracketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/bracket")
 public class BracketController {
 
     @Autowired
