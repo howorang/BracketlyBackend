ce78af6682de0f7b09671c8433750f8075ebb9dc
howorang <howorang@gmail.com>
2017-12-16 17:12:21 +0100

    Database config and naming fix

--- src/main/java/edu/bracketly/backend/model/entity/user/User.java
+++ src/main/java/edu/bracketly/backend/model/entity/user/User.java
@@ -1,16 +1,20 @@
 package edu.bracketly.backend.model.entity.user;
 
 import edu.bracketly.backend.model.entity.BaseEntity;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Entity
+@NoArgsConstructor
+@Table(name = "user_t")
 public class User extends BaseEntity {
+
     private String username;
 
     private String password;
