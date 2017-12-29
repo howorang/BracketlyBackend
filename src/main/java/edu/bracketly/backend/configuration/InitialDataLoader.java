b64d26f9d05673a2d51ef79993f248d87584ea68
howorang <howorang@gmail.com>
2017-12-30 00:08:54 +0100

    New initial data loader

--- src/main/java/edu/bracketly/backend/configuration/InitialDataLoader.java
+++ src/main/java/edu/bracketly/backend/configuration/InitialDataLoader.java
@@ -1,35 +1,67 @@
 package edu.bracketly.backend.configuration;
 
+import edu.bracketly.backend.model.entity.BRACKET_TYPE;
+import edu.bracketly.backend.model.entity.SEEDING_STRATEGY;
+import edu.bracketly.backend.model.entity.Tournament;
 import edu.bracketly.backend.model.entity.user.Player;
-import edu.bracketly.backend.repository.UserRepository;
+import edu.bracketly.backend.model.flow.TOURNAMENT_STATUS;
+import edu.bracketly.backend.repository.PlayerRepository;
+import edu.bracketly.backend.repository.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class InitialDataLoader implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
-    private UserRepository userRepository;
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private TournamentRepository tournamentRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        if (userRepository.existsByUsername("test")) return;
+        if (playerRepository.existsByUsername("test")) return;
         Player test = new Player();
         test.setUsername("test");
         test.setPassword(passwordEncoder.encode("test"));
         ;
-        userRepository.save(test);
+        playerRepository.save(test);
 
         Player howo = new Player();
         howo.setUsername("howo");
         howo.setPassword(passwordEncoder.encode("test"));
-        userRepository.save(howo);
+        playerRepository.save(howo);
+
+        Set<Player> players = new HashSet<>();
+        for (int i = 0; i < 8; i++) {
+            Player player = new Player();
+            player.setUsername("test" + i);
+            player.setPassword(passwordEncoder.encode("test" + i));
+            playerRepository.save(player);
+            players.add(playerRepository.findByUsername(player.getUsername()));
+        }
+
+        Tournament tournament = new Tournament();
+        tournament.setName("evo");
+        tournament.setSeeding_strategy(SEEDING_STRATEGY.RANDOM);
+        tournament.setCreationDate(new Date());
+        tournament.setEventDate(new Date());
+        tournament.setBracketType(BRACKET_TYPE.SINGLE_ELIMINATION);
+        tournament.setOrganizer(playerRepository.findByUsername("test"));
+        tournament.setPlayers(players);
+        tournament.setStatus(TOURNAMENT_STATUS.PLANNING);
+        tournamentRepository.save(tournament);
     }
 
 }
--- src/main/java/edu/bracketly/backend/repository/PlayerRepository.java
+++ src/main/java/edu/bracketly/backend/repository/PlayerRepository.java
@@ -4,4 +4,7 @@ import edu.bracketly.backend.model.entity.user.Player;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PlayerRepository extends PagingAndSortingRepository<Player, Long> {
+    Player findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
