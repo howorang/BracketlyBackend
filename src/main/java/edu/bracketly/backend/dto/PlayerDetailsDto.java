191175a8e5ff6f487f5c70ed88b218eaf5beda2e
howorang <howorang@gmail.com>
2017-12-29 03:27:35 +0100

    dto fix

--- src/main/java/edu/bracketly/backend/dto/PlayerDetailsDto.java
+++ src/main/java/edu/bracketly/backend/dto/PlayerDetailsDto.java
@@ -2,12 +2,18 @@ package edu.bracketly.backend.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class PlayerDetailsDto extends PlayerDto {
     List<TournamentDto> tournaments;
     List<MatchDto> liveMatches;
+
+    public PlayerDetailsDto() {
+        type = PlayerDetailsDto.class.getSimpleName();
+    }
 }
